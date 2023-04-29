5f37bc173afc362ee22b6ca80725a594f4ba6329
larestrepo <larestrepomu@gmail.com>
2023-04-28 21:40:22 -0500

    First integration with postgres and table creation

---
+++ models/dblib.py
@@ -0,0 +1,50 @@
+from configparser import ConfigParser
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+config_path = "./models/credentials.local.ini"  # Optional argument
+
+
+def config(config_path: str, section: str) -> dict:
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(config_path)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            "Section {0} not found in the {1} file".format(section, config_path)
+        )
+
+    return db
+
+
+""" Connect to the PostgreSQL database server using SQLAlchemy method """
+# read connection parameters
+params = config(config_path, section="postgresql")
+
+# connect to the PostgreSQL server
+print("Connecting to the PostgreSQL database...")
+conn_string = f"postgresql://{params['user']}:{params['password']}@{params['host']}:{params['port']}/{params['database']}"
+engine = create_engine(conn_string, connect_args={}, future=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+
+Base = declarative_base()
+connection = engine.connect()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ models/dbmodels.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from sqlalchemy import BigInteger, Boolean, Column, Enum
+from sqlalchemy import ForeignKey, Integer, String, Text, DateTime, BigInteger
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID, JSON
+
+from .dblib import Base
+from .mixins import Timestamp
+import uuid
+
+# from routers.api_v1.endpoints.pydantic_schemas import ScriptPurpose
+
+
+class Projects(Timestamp, Base):
+    __tablename__ = "projects"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    name = Column(Text, nullable=False)
+    country = Column(Text, nullable=True)
+    sector = Column(Text, nullable=True)
+    url = Column(Text, nullable=True)
+    owner = Column(Text, nullable=True)
+    uid = Column(Text, nullable=True)
+    kind = Column(Text, nullable=False)
+    asset_type = Column(Text, nullable=False)
+    version_id = Column(Text, nullable=False)
+    date_created = Column(DateTime, nullable=False)
+
+    # user = relationship("User", back_populates="wallet")
+    # transactions = relationship("Transactions", back_populates="wallet")
