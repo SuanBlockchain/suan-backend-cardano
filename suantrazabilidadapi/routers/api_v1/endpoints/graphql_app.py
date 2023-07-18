d12662f65e1b2e56a15899537c0d43cff110f9e0
larestrepo <larestrepomu@gmail.com>
2023-07-18 02:26:53 -0500

    major updates to create project

--- suantrazabilidadapi/routers/api_v1/endpoints/graphql_app.py
+++ suantrazabilidadapi/routers/api_v1/endpoints/graphql_app.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 import json
+from sqlalchemy.orm import Session
+import pandas as pd
+from sqlalchemy import or_, not_
 
+from suantrazabilidadapi.db.dblib import get_db
 from suantrazabilidadapi.utils.graphqlClass import Plataforma
 from suantrazabilidadapi.kobo.manager import Manager
 from suantrazabilidadapi.routers.api_v1.endpoints import pydantic_schemas
+from suantrazabilidadapi.db.models import dbmodels
 
 router = APIRouter()
 
@@ -34,7 +39,13 @@ def get_item_from_graphql(command_name: pydantic_schemas.Form) -> dict:
         summary="Put register data in Plataforma for related projects",
         response_description="Succesfully project created"
         )
-def put_project() -> dict:
+def put_project(db: Session = Depends(get_db)) -> dict:
+
+    name_list = []
+    format_list = []
+    proyecto_list = []
+    featureName_list = []
+    featureType_list = []
 
     form_id = "avJvoP4AH7Kj2LgVrdwdpj"
     
@@ -44,23 +55,77 @@ def put_project() -> dict:
     form.fetch_data()
     df = form.data
 
-    df_json = json.loads(df.to_json())
+    if df is not None:
+        # Check the format of features to integrate between kobo and Plataforma app
+        query = db.query(dbmodels.Principalform).filter(dbmodels.Principalform.format != "NaN")
+        results = query.all()
 
-    _id = df_json["_id"]
-    name_dict = df_json["A_asset_names"]
-    description_dict = df_json["A_description"]
-    categoryID_dict = df_json["A_category"]
-    A_postulante_name = df_json["A_postulante_name"]["0"]
+        for result in results:
+            name_list.append(result.name)
+            proyecto_list.append(result.proyecto)
+            featureName_list.append(result.featureName)
+            featureType_list.append(result.featureType)
+            format_list.append(result.format)
 
-    plataforma = Plataforma()
-    for k, v in _id.items(): #TODO: 
-        result = plataforma.getProjects(v)
-        if result["data"]["getProduct"] is None:
-            result = plataforma.createProject(v, name_dict, categoryID_dict["0"], A_postulante_name)
+        data_list = df.to_dict(orient="records")
 
-    #TODO: pack the name_dict and description_dict into a new dict with both name and description ready
+        # Filter out NaN values from dictionaries
+        filtered_list = [
+            {key: value for key, value in d.items() if pd.notnull(value)}
+            for d in data_list
+        ]
 
+        for data in filtered_list:
+            project_id = data["_id"]
+            project = Plataforma().getProjects(project_id)
+            
+            if project["data"]["data"]["getProduct"] is None:
 
-    # result = plataforma.createProject(name_dict, categoryID, isActive)
+                # Values specific to the project table
+                project_name = data["A_asset_names"]
+                project_description = data["A_description"]
+                project_category = data["A_category"]
 
-    return {"results": result}
+                # Values specific to the product features
+                fixed_index = 0
+                filtered_data = {}
+                for k, v in data.items():
+                    try:
+                        index = name_list.index(k)
+                        if format_list[index] != "Json" and v is not None:
+                            filtered_data[featureName_list[index]] =  v
+                        elif format_list[index] == "Json" and k == featureName_list[index]:
+                            fixed_index = 0
+                            someJson_list = []
+                            someJson_list.append({
+                                k: v
+                            })
+                            someJson_dict = { k: [{ k: v}]}
+                            fixed_index = index
+                        elif format_list[index] == "Json" and k != featureName_list[index]:
+                            someJson_list.append({ k: v })
+                            someJson_dict[featureName_list[fixed_index]] = someJson_list
+                        else:
+                            print("Nothing to update in the dictionary")
+
+                        if fixed_index != 0:
+                            filtered_data.update(someJson_dict)
+
+                    except ValueError:
+                        next
+
+                response = Plataforma().createProject(project_id, project_name, project_description, project_category, filtered_data)
+            
+            else:
+                response = {
+                    "success": True,
+                    "msg": "Project already exists in DynamoDB",
+                    "data": project["data"]
+                }
+
+    else:
+        response = {
+            "success": True,
+            "data": "Form data is empty"
+        }
+    return {"results": response}
