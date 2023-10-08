4c6b76cf655e500d1218577d5912e732fb3da8e8
larestrepo <larestrepomu@gmail.com>
2023-10-07 22:04:20 -0500

    qrdb ledger and table creation

--- suantrazabilidadapi/routers/api_v1/api.py
+++ suantrazabilidadapi/routers/api_v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from .endpoints import kobo_projects, db_data, projects, sandbox
+from .endpoints import kobo_projects, db_data, projects, sandbox, qrdb
 
 api_router = APIRouter()
 api_router.include_router(kobo_projects.router, prefix="/kobo", tags=["Kobo"])
 api_router.include_router(db_data.router, prefix="/data", tags=["Data"])
 api_router.include_router(projects.router, prefix="/projects", tags=["Projects"])
 api_router.include_router(sandbox.router, prefix="/sandbox", tags=["Sandbox"])
+api_router.include_router(qrdb.router, prefix="/qrdb", tags=["QRDB"])
